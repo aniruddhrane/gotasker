f92c00db4c185dd68cc70abe6ff88bd096920cd5
aniruddhrane <aniruddhrane5@gmail.com>
2026-07-21 16:30:37 +0530

    feat: implement in-memory job queue (enqueue and dequeue)

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -3,3 +3,22 @@ import "github.com/aniruddhrane/gotasker/internal/job"
 type Queue struct{
 	jobs chan job.Job
 }
+
+func New(size int)*Queue{
+  q:= Queue{
+	jobs:make(chan job.Job,size)
+  }
+  return &q
+}
+
+func (q*Queue) Enqueue(job job.Job){
+    q.jobs <- job
+	// It sends the job value into the jobs channel. If the channel has available buffer space, 
+	// the send completes immediately. If the buffer is full, the sending goroutine blocks until space becomes available
+}
+
+func(q*Queue) Dequeue() job.Job{
+	 value := <- q.jobs
+    return value
+	//the worker waits till the channel has something when it has something it can remove it
+}
